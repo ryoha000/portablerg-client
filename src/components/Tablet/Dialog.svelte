a5d44e64b5e52356646e6fd2baae550a3abdbb28
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-10-17 14:17:11 +0900

    add Dialog(slot)

---
+++ src/components/Tablet/Dialog.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  export let style: string = ''
+
+  const dispatch = createEventDispatcher();
+  const closeToggleSetting = (e: Event) => {
+    e.stopPropagation()
+    dispatch('close')
+  }
+</script>
+
+<style>
+  .layer {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 300;
+    position: absolute;
+  }
+  .settingContainer {
+    background-color: white;
+    position: absolute;
+    z-index: 999999;
+    border: rgba(0, 0, 0, 0.7) solid 1px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+
+<div class="layer" on:click="{closeToggleSetting}"></div>
+<div class="settingContainer" style="{style}">
+  <slot></slot>
+</div>
